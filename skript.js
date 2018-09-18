bfd118879dcc2b2385de02b717ae8166339fc05c
Annika100 <annika.fischer97@web.de>
2018-09-18 16:44:48 +0200

    Add files via upload

---
+++ skript.js
@@ -0,0 +1,35 @@
+/*AFRAME.registerComponent('hello-world', {
+    init: function () {
+        console.log('Hello, World!');
+    }
+});*/
+
+// HTML: <a-entity hello-world></a-entity>
+
+
+
+/*AFRAME.registerComponent('log', {
+    schema: {
+        message: {type: 'string', default: 'Hello, World!'}
+    },
+
+    init: function () {
+        console.log(this.data.message); // component’s property type values are available through this.data
+    }
+}); */
+
+// HTML: <a-entity log="message: Hello, Metaverse!"></a-entity>
+
+
+// Component to change to a sequential color on click.
+AFRAME.registerComponent('cursor-listener', {
+    init: function () {
+        var lastIndex = -1;
+        var COLORS = ['red', 'green', 'blue'];
+        this.el.addEventListener('click', function (evt) {
+            lastIndex = (lastIndex + 1) % COLORS.length;
+            this.setAttribute('material', 'color', COLORS[lastIndex]);
+            console.log('I was clicked at: ', evt.detail.intersection.point);
+        });
+    }
+});
