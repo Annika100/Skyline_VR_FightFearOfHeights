c6915af5e0e4a678519542217c26863c80e00b52
Annika100 <annika.fischer97@web.de>
2018-09-25 11:46:35 +0200

    Update skript.js

--- skript.js
+++ skript.js
@@ -21,8 +21,8 @@
 // HTML: <a-entity log="message: Hello, Metaverse!"></a-entity>
 
 
-// Component to change to a sequential color on click.
-AFRAME.registerComponent('cursor-listener', {
+// Component to change to a sequential color on click/ hover.
+/*AFRAME.registerComponent('cursor-listener', {
     init: function () {
         var lastIndex = -1;
         var COLORS = ['red', 'green', 'blue'];
@@ -33,3 +33,35 @@ AFRAME.registerComponent('cursor-listener', {
         });
     }
 });
+*/
+
+
+/* Sound
+AFRAME.registerComponent('audiohandler', {
+    init: function() {
+        var playing = false;
+        var audio = document.querySelector("[wave]");
+        this.el.addEventListener('click', {
+            if (!playing) {
+            audio.play();
+        }
+        else {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+        playing = !playing;
+        });
+    }
+});
+*/
+
+
+*/ Sound */
+
+var audi = document.getElementById('audi');
+console.log(audi);
+
+document.getElementById('play-button').addEventListener("click", function(e){
+    this.style.display = 'none';
+    audi.play();
+}, false);
